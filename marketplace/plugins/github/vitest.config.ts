4bacdeb6e4ca7621bd74bce258c6f6b5d77e789d
Sawyer Hood <sawyerjhood@gmail.com>
2026-07-14 21:15:21 -0700

    test(plugins): consume packaged SDK exports

--- marketplace/plugins/github/vitest.config.ts
+++ marketplace/plugins/github/vitest.config.ts
@@ -1,15 +1,6 @@
-import path from "node:path";
 import { defineWorkspaceTestConfig } from "../../../vitest.shared.js";
 
 export default defineWorkspaceTestConfig({
-  resolve: {
-    alias: {
-      "@bb/plugin-sdk": path.resolve(
-        import.meta.dirname,
-        "../../../packages/plugin-sdk/src",
-      ),
-    },
-  },
   test: {
     silent: "passed-only",
     name: "bb-plugin-github",
