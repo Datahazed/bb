a55cbdb37e6aa8e2c868a9fa838bb74da033d6cf
Bersabel Tadesse <brsabel.t@gmail.com>
2026-08-31 15:50:20 -0700

    Integrate experimental sidebar foundation

--- apps/app/src/components/sidebar/AppSidebar.tsx
+++ apps/app/src/components/sidebar/AppSidebar.tsx
@@ -209,7 +209,6 @@ export function AppSidebar({
           : undefined
       }
       onProjectSelect={closeOnMobile}
-      onSplit={onSplit}
       isCreatingProject={quickCreateProject.isCreating}
     />
   );
